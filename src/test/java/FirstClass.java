808be7df767600a115065bf7562b7779398f110b
nickamir <nekruz.eco.tj@gmail.com>
2022-02-24 19:13:57 -0800

    second commit

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,5 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hi World");
+        System.out.println("sup");
+        System.out.println("Babyy");
     }
 }
